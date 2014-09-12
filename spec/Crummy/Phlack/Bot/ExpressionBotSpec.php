4685f661af68ec39403a13f5aaa3803b58565da8
Michael Crumm <mike.crumm@groundctrl.com>
2014-09-11 18:17:54 -0700

    Updating ExpressionBot

--- spec/Crummy/Phlack/Bot/ExpressionBotSpec.php
+++ spec/Crummy/Phlack/Bot/ExpressionBotSpec.php
@@ -2,6 +2,8 @@
 
 namespace spec\Crummy\Phlack\Bot;
 
+use Crummy\Phlack\Common\Iterocitor;
+use Crummy\Phlack\WebHook\Reply\Reply;
 use Crummy\Phlack\WebHook\SlashCommand;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -10,9 +12,9 @@ use Symfony\Component\ExpressionLanguage\SyntaxError;
 
 class ExpressionBotSpec extends ObjectBehavior
 {
-    function let($name, ExpressionLanguage $language)
+    function let($name, ExpressionLanguage $language, Iterocitor $iterocitor)
     {
-        $this->beConstructedWith($name, $language);
+        $this->beConstructedWith($name, $language, $iterocitor);
     }
 
     function it_extends_abstract_bot()
@@ -21,30 +23,35 @@ class ExpressionBotSpec extends ObjectBehavior
         $this->shouldBeAnInstanceOf('\Crummy\Phlack\Bot\AbstractBot');
     }
 
-    function it_evaluates_expressions($language, SlashCommand $command)
+    function it_evaluates_expressions($language, $iterocitor, SlashCommand $command, Reply $reply)
     {
-        $command->getText()->willReturn('2 + 2');
+        $iterocitor->say('4')->willReturn($reply);
+        $reply->offsetGet('text')->willReturn('4');
+
+        $command->offsetGet('text')->willReturn('2 + 2');
         $language->evaluate('2 + 2', [])->willReturn('4');
 
         $this
-            ->execute($command)
-                ->get('text')
-                    ->shouldReturn('4')
+            ->execute($command)['text']
+                ->shouldReturn('4')
         ;
     }
 
-    function it_returns_the_syntax_error_as_the_response_in_bad_commands($language, SlashCommand $command)
+    function it_returns_the_syntax_error_as_the_response_in_bad_commands($language, SlashCommand $command, $iterocitor, Reply $reply)
     {
-        $command->getText()->willReturn('foo + 2');
+        $command->offsetGet('text')->willReturn('foo + 2');
 
         $errorMessage   = 'Variable "foo" is not valid around position 1.';
         $error          = new SyntaxError('Variable "foo" is not valid', 1);
+
+        $iterocitor->say($errorMessage)->willReturn($reply);
+        $reply->offsetGet('text')->willReturn($errorMessage);
+
         $language->evaluate('foo + 2', [])->willThrow($error);
 
         $this
-            ->execute($command)
-            ->get('text')
-            ->shouldReturn($errorMessage);
+            ->execute($command)['text']
+                ->shouldReturn($errorMessage);
         ;
     }
 }
